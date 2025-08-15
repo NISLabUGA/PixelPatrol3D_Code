a25631cfbc23f503a39296debe4446c271ecacc6
sking115422 <spencerd.king@gmail.com>
2025-08-15 16:03:28 -0700

    minor adjustments

--- pp_discover/pp_crawler/config.js
+++ pp_discover/pp_crawler/config.js
@@ -47,7 +47,7 @@ const chrome_dir = "/home/user/chrome_user";
 // var home_dir = "./pp_crawler_baseline/";
 
 // For docker use
-var home_dir = "../";
+var home_dir = "./";
 
 var MAIN_LOGS_DIR =
   home_dir +
